66cd8fe804b9ac4b094893373e00ddf608a23def
NgvanPhux <vanphuc09035@gmail.com>
2024-03-11 01:10:06 +0700

    bai3

--- sever.js
+++ sever.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+require('dotenv').config();
+// de sd path const path= require('path');
+const app = express();
+const port = process.env.PORT || 8080;// port   => hardcode ( bien cung khi thay doi phai doi nhieu > can file env)
+const hostname = process.env.HOST_NAME;
 // config template engine 
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
+app.set('views', './src/views/');
+// app.set('views',path.join(__dirname,'view'));
+app.set('view engine', 'ejs');
 // khai bao router
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
 app.get('/abcd', (req, res) => {
     res.render('sample.ejs')
-})
-app.listen(port, () => {
+});
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
