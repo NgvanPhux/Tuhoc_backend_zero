7bde8a8b7c0b83ba981018a545c4841caca53a95
NgvanPhux <vanphuc09035@gmail.com>
2024-03-12 11:29:10 +0700

    bai1221

--- sever.js
+++ sever.js
@@ -9,6 +9,8 @@ const path = require('path');
 const { config } = require('process');
 const router = require('./src/route/web');
 const webroute = require('./src/route/web');
+const mysql = require('mysql2');
+
 
 
 // config template engine
@@ -16,6 +18,25 @@ configviewengine(app);
 
 // khai bao router  
 app.use('/ver1', webroute);
+
+//TEST CONNETTION MYSQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '121025',
+    database: 'webver1',
+});
+
+connection.query(
+    'SELECT *FROM  ver1 u',
+    function (err, results, fields) {
+        console.log(">>>>>>>>> results =", results);
+        console.log(">>>>>>> fields    =", fields);
+    }
+);
+
+
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
